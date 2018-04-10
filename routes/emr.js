2eb4be6be926f2c6a4fcaa65d221236ac1989e32
Tyler Nunez <W0532191@selu.edu>
2018-04-09 19:45:59 -0500

    added get all, working on get by id

--- routes/emr.js
+++ routes/emr.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var fs = require("fs");
 
-router.route('/')
-    .get(function (req,res){
-        res.send('Get All EMRs')
-    })
-    .post(function (req,res){
-        res.send('Add EMR')
-    })
+router.get('/', function(req, res, next) {
+    var contents = fs.readFileSync("public/EMR.json");
+    res.send(contents);
+});
+
+router.get('/:id', function(req, res, next) {
+    var contents = fs.readFileSync("public/EMR.json");
+    var parsed = JSON.parse(contents);
+    res.send(parsed.EMR[id].firstName + " " + parsed.EMR[id].lastName );
+});
 
 module.exports = router;
