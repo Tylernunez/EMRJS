2ebae730cc83131475a550c88e35d25b56350af7
Tyler Nunez <W0532191@selu.edu>
2018-04-21 14:18:05 -0500

    Making further changes to test deployment

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ router.put("/:_id", function(req, res) {
 
     db.collection(EMR_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
         if (err) {
-            handleError(res, err.message, "Failed to update contact");
+            handleError(res, err.message, "Failed to update EMR");
         } else {
             updateDoc._id = req.params.id;
             res.status(200).json(updateDoc);
@@ -100,9 +100,9 @@ router.put("/:_id", function(req, res) {
 
 router.delete("/:_id", function(req, res) {
 
-    db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+    db.collection(EMR_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
         if (err) {
-            handleError(res, err.message, "Failed to delete contact");
+            handleError(res, err.message, "Failed to delete EMR");
         } else {
             res.status(200).json(req.params.id);
         }
